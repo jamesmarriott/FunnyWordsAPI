fec4877505b53ed561c0ecc51092551bfeaad17a
James Marriott <sawheadproductions@gmail.com>
2021-04-30 11:31:55 +0200

    cors added

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 const express = require('express')
+const cors = require ('cors')
 const app = express()
 const impData = require('./words.json')
+const allowedOrigins = ['http://localhost:3000'];
 let port = process.env.PORT || 3000
+
+app.use(cors())
+
+app.get("/", (req, res) => {
+  res.send("Please check the documentation at https://github.com/jamesmarriott/FunnyWordsAPI to use the Funny Words API")
+})
+
+app.get('/products/:id', function (req, res, next) {
+  res.json({msg: 'This is CORS-enabled for all origins!'})
+})
 
 app.get("/api/words", (req, res) => {
   res.send(impData)
